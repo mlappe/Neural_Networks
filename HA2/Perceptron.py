d6ff277987a4faf8659561b5281c7f43acf3b502
MaxAdmin <lappe@cl.uni-heidelberg.de>
2016-11-10 18:22:10 +0100

    added DataREader class

--- HA2/Perceptron.py
+++ HA2/Perceptron.py
@@ -1,8 +1,34 @@
+from collections import namedtuple
+import numpy
 
 
+
+Datapoint = namedtuple('Datapoint', ['features', 'goldlabel'])
+
+class DataReader():
+
+	def __init__(self,filename):
+		self.filename = filename
+
+	def __iter__(self):
+		"""
+		iterates over all datapoints in the file
+		yields Datapoint objects, not the string
+		"""
+		with open(self.filename) as f:
+			for line in f:
+				line = line.strip()
+				line = line.split(" ")
+				features = [float(dim.split(":")[1]) for dim in line[1:]]
+				yield Datapoint(numpy.array(features),line[0])
 
 class Perceptron():
 	pass
 
 if __name__ == "__main__":
-	pass
+	dataset = DataReader("data/iris.setosa-v-rest.train")
+	for line in dataset:
+		print(line)
+
+	print(list(dataset))
+	
