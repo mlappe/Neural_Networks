3d2146c6ffb65778620b82172602855c2143bf10
MaxAdmin <lappe@cl.uni-heidelberg.de>
2016-11-10 22:36:29 +0100

    added PErceptron class

--- HA2/Perceptron.py
+++ HA2/Perceptron.py
@@ -17,18 +17,48 @@ class DataReader():
 		"""
 		with open(self.filename) as f:
 			for line in f:
+
 				line = line.strip()
 				line = line.split(" ")
 				features = [float(dim.split(":")[1]) for dim in line[1:]]
-				yield Datapoint(numpy.array(features),line[0])
+
+				yield Datapoint(numpy.array(features),numpy.float32(line[0]))
+
+def constantlearningrate():
+	yield 1
 
 class Perceptron():
-	pass
+	def __init__(self,dimensions,*,init_weights = "zeros",init_bias = numpy.float32(0.0),learningrate = constantlearningrate()):
+		
+
+		if init_weights == "zeros":
+			self.weights = numpy.zeros(dimensions)
+		else:
+			self.weights = init_weights
+		assert type(self.weights) is numpy.ndarray
+
+		assert type(init_bias) is numpy.float32
+		self.bias  = init_bias
+
+		self.learningrate = learningrate
+
+	def predict(self,datapoint):
+		return numpy.dot(datapoint.features,self.weights) + self.bias
+
+	def update(self,datapoints):
+		learningrate = next(self.learningrate)
+		
+		updates = [(datapoint.goldlabel - self.predict(datapoint)) * datapoint.features * learningrate for datapoint in datapoints]
+
+		self.weights += sum(updates)
 
 if __name__ == "__main__":
+
 	dataset = DataReader("data/iris.setosa-v-rest.train")
+
 	for line in dataset:
 		print(line)
 
-	print(list(dataset))
+	list(dataset)
 	
+	print(Perceptron(5).bias)
